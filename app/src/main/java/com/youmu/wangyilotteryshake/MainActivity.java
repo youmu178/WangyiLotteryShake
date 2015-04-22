0f75e50f11faf14a5800ad9857848c56f8abc502
youmu178 <youmu178@sina.com>
2015-04-22 18:12:55 +0800

    shake

--- app/src/main/java/com/youmu/wangyilotteryshake/MainActivity.java
+++ app/src/main/java/com/youmu/wangyilotteryshake/MainActivity.java
@@ -1,19 +1,170 @@
 package com.youmu.wangyilotteryshake;
 
-import android.support.v7.app.ActionBarActivity;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.Random;
 
-public class MainActivity extends ActionBarActivity {
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class MainActivity extends ActionBarActivity implements ShakeListener.OnShakeListener {
+    @InjectView(R.id.shake_tv)
+    TextView shakeTv;
+    @InjectView(R.id.shake_img1)
+    ImageView shakeImg1;
+    @InjectView(R.id.shake_img2)
+    ImageView shakeImg2;
+    @InjectView(R.id.shake_img3)
+    ImageView shakeImg3;
+    @InjectView(R.id.shake_layout)
+    LinearLayout shakeLayout;
+    private SoundPool soundPool;
+
+    private ShakeListener shakeListener;
+    private Handler mHandler = new Handler();
+    private HashMap<Integer, Integer> aduioMap = new HashMap<Integer, Integer>();
+    private Random mRandom = new Random();
+    private int mImgs[] = {R.mipmap.dice1, R.mipmap.dice2, R.mipmap.dice3, R.mipmap.dice4, R.mipmap.dice5, R.mipmap.dice6};
+    private int mImgsAnim[] = {R.mipmap.dice_f1, R.mipmap.dice_f2, R.mipmap.dice_f3, R.mipmap.dice_f4};
+    private int mCount;
+    private boolean isStopAnim = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        initAudio();
+
+        shakeListener = new ShakeListener(this);
+        shakeListener.setOnShakeListener(this);
     }
 
+
+    private void initAudio() {
+//        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP) {
+//            SoundPool.Builder builder = new SoundPool.Builder();
+//        }
+
+        soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        aduioMap.put(1, soundPool.load(this, R.raw.rotate, 1));
+        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+
+            }
+        });
+    }
+
+    private void randomImg(ImageView imageView) {
+        int num = mRandom.nextInt(6);
+        if (num == 0) {
+            num = 1;
+        }
+        mCount += num;
+        imageView.setImageResource(mImgs[num-1]);
+    }
+
+    private void randomImgAnim(final ImageView imageView) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(!isStopAnim) {
+                    int num = mRandom.nextInt(4);
+                    imageView.setImageResource(mImgsAnim[num]);
+                }
+            }
+        }).start();
+
+    }
+
+    @Override
+    public void onShake() {
+        shakeListener.stop();
+        play();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+//                anim();
+                showRadome();
+                shakeListener.start();
+            }
+        }, 600);
+    }
+
+    private void anim() {
+        AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f, 1.0f);
+        alphaAnimation.setDuration(2000);
+        shakeLayout.startAnimation(alphaAnimation);
+        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                isStopAnim = false;
+                Log.i("youzh", "onAnimationStart: " + isStopAnim);
+                randomImgAnim(shakeImg1);
+//                randomImgAnim(shakeImg2);
+//                randomImgAnim(shakeImg2);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                isStopAnim = true;
+                Log.i("youzh", "onAnimationEnd: " + isStopAnim);
+                showRadome();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+    private void showRadome() {
+        randomImg(shakeImg1);
+        randomImg(shakeImg2);
+        randomImg(shakeImg3);
+        Toast.makeText(MainActivity.this, "总数为：" + mCount, Toast.LENGTH_SHORT).show();
+        mCount = 0;
+    }
+
+    public void play() {
+        if (soundPool != null) {
+            soundPool.play(aduioMap.get(1), 1.0f, 1.0f, 0, 0, 1);
+        }
+    }
+
+    public void stop() {
+        if (soundPool != null) {
+            soundPool.release();
+
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (soundPool != null) {
+            soundPool.release();
+            soundPool = null;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +187,6 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
